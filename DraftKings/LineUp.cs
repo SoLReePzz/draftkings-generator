3a24f30e15a60839bd73b25c6076d69218c5423b
samueldavidcrawford <samueldavidcrawford1@gmail.com>
2016-08-09 21:00:42 -0500

    - Broke some stuff in lineUp.cs

--- DraftKings/LineUp.cs
+++ DraftKings/LineUp.cs
@@ -10,7 +10,7 @@ namespace DraftKings
     {
         //set the min cost of the lineUp as you so choose
         private int _maxCost = 50000;
-        private int _minCost = 40000;
+        private int _minCost = 46500;
         private List<List<Player>> _lineUps;
      
 
@@ -23,57 +23,74 @@ namespace DraftKings
             for (var i = 0; i < playerMatrix[0].Count(); i++) //qb's
             {
                 lineUp.Add(playerMatrix[0][i]);
-                var qBName = playerMatrix[0][i].Name;
                 for (var j = 0; j < playerMatrix[1].Count(); j++) //rb1's
                 {
                     lineUp.Add(playerMatrix[1][j]);
-                    var rBName = playerMatrix[1][j].Name;
-                    for (var k=0; k < playerMatrix[2].Count(); k++) //wr1's
+                    for (var l = 0; l < playerMatrix[1].Count(); l++) //rb2's
                     {
-                        lineUp.Add(playerMatrix[2][k]);
-                        var wrName = playerMatrix[2][k].Name;
-                        for (var ii=0; ii< playerMatrix[3].Count(); ii++) //te's
+                        lineUp.Add(playerMatrix[1][l]);
+                        for (var k = 0; k < playerMatrix[2].Count(); k++) //wr1's
                         {
-                            lineUp.Add(playerMatrix[3][ii]);
-                            for (var jj=0; jj< playerMatrix[4].Count(); jj++) //dst's
+                            lineUp.Add(playerMatrix[2][k]);
+                            for (var ll = 0; ll < playerMatrix[2].Count(); ll++) //wr2's
                             {
-                                lineUp.Add(playerMatrix[4][jj]);
-                                for (var kk = 0; kk < playerMatrix[5].Count(); kk++) //flex's
+                                lineUp.Add(playerMatrix[2][ll]);
+                                for (var w = 0; w < playerMatrix[2].Count(); w++) //wr3's
                                 {
-                                    lineUp.Add(playerMatrix[5][kk]);
-                                    var flexName = playerMatrix[5][kk].Name;
-                                    var totalCost = 0;
-                                    foreach (var player in lineUp)
+                                    lineUp.Add(playerMatrix[2][w]);
+                                    for (var ii = 0; ii < playerMatrix[3].Count(); ii++) //te's
                                     {
-                                        totalCost = player.Cost + totalCost;
-                                    }
-                                    if (totalCost <= _maxCost && totalCost >= _minCost)
-                                    {
-                                        _lineUps.Add(lineUp);
-                                    }
-                                    lineUp.Remove(playerMatrix[5][kk]);
+                                        lineUp.Add(playerMatrix[3][ii]);
 
+                                        for (var jj = 0; jj < playerMatrix[4].Count(); jj++) //dst's
+                                        {
+                                            lineUp.Add(playerMatrix[4][jj]);
+
+                                            for (var kk = 0; kk < playerMatrix[5].Count(); kk++) //flex's
+                                            {
+                                                lineUp.Add(playerMatrix[5][kk]);
+                                                var flexName = playerMatrix[5][kk].Name;
+                                                var totalCost = 0;
+                                                foreach (var player in lineUp)
+                                                {
+                                                    totalCost = player.Cost + totalCost;
+                                                }
+                                                if (totalCost <= _maxCost && totalCost >= _minCost)
+                                                {
+                                                    
+                                                        _lineUps.Add(lineUp);
+                                                }
+                                                lineUp.Remove(playerMatrix[5][kk]); //remove flex
+                                            }
+                                            lineUp.Remove(playerMatrix[4][jj]); //remove DST
+                                        }
+                                        lineUp.Remove(playerMatrix[3][ii]); //remove te
+                                    }
+                                    lineUp.Remove(playerMatrix[2][w]); //remove wr3
                                 }
-                                lineUp.Remove(playerMatrix[4][jj]);
+                                lineUp.Remove(playerMatrix[2][ll]); //remove wr2
                             }
-                            lineUp.Remove(playerMatrix[3][ii]);
+                            lineUp.Remove(playerMatrix[2][k]); //remove wr1
                         }
-                        lineUp.Remove(playerMatrix[2][k]);
+                        lineUp.Remove(playerMatrix[1][l]); //remove rb2
                     }
-                    lineUp.Remove(playerMatrix[1][j]);
+                    lineUp.Remove(playerMatrix[1][j]); //remove rb1
                 }
-                lineUp.Remove(playerMatrix[0][i]);
+                lineUp.Remove(playerMatrix[0][i]); //remove qb
             }
 
-            for (var i = 0; i < _lineUps.Count(); i++)
-            {
-                Console.WriteLine("Lineup number: " + i);
-                for (var j=0; j < _lineUps[i].Count(); j++)
-                {
-                    Console.WriteLine(_lineUps[i][j].Name);
-                }
+            Console.WriteLine("Number of Lineups: " + _lineUps.Count());
+            
+            //for (var i = 0; i < _lineUps.Count(); i++)
+            //{
+            //    Console.WriteLine("Lineup number: " + i);
+            //    for (var j=0; j < _lineUps[i].Count(); j++)
+            //    {
+            //        Console.WriteLine(_lineUps[i][j].Name);
+            //    }
                     
-            }
+           // }
+
             return _lineUps;
         }
 
--- DraftKings/PlayersByPosition.cs
+++ DraftKings/PlayersByPosition.cs
@@ -18,9 +18,9 @@ namespace DraftKings
 
         //Some players do not see the field because they suck (relatively) 
         //Set the cutoff cost to exclude these degenerates from our lineups
-        private int _qBCutoffCost = 5000;
-        private int _rBCutoffCost = 3000;
-        private int _wRCutoffCost = 3000;
+        private int _qBCutoffCost = 5600;
+        private int _rBCutoffCost = 4000;
+        private int _wRCutoffCost = 4000;
         private int _tECutoffCost = 3000;
         private int _dSTCutoffCost = 1000;
 
